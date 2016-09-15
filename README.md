9ebd09bb8c0a68f3949fee87a0caef1fcedd3c45
David DAUGREILH <david.daugreilh@globalis-ms.com>
2016-09-15 14:14:01 +0200

    Update mu plugin

--- README.md
+++ README.md
@@ -4,28 +4,10 @@ When a new user is added in one site, add it to all the network sites
 
 ## Installation
 
-You can install this plugin via the command-line or the WordPress admin panel.
+Install the WordPress mu-plugin:
 
-## via Command-line
-
-1. Download the [latest zip](https://github.com/wp-globalis-tools/wpg-multisite-share-users/archive/master.zip) of this repo.
-2. Add the folder in your plugins directory (wp-content/plugins)
-3. Activate the plugin via [wp-cli](http://wp-cli.org/commands/plugin/activate/).
-
-```sh
-wp plugin activate wpg-multisite-share-users
-```
-
-## via WordPress Admin Panel
-
-1. Download the [latest zip](https://github.com/wp-globalis-tools/wpg-multisite-share-users/archive/master.zip) of this repo.
-2. In your WordPress admin panel, navigate to Plugins->Add New
-3. Click Upload Plugin
-4. Upload the zip file that you downloaded.
-
-## Configuration
-
-Define MSU_MAX_SITES in your config file :
+1. Add the file wpg-multisite-share-users.php in your wp-content/mu-plugins directory
+2. (Optional) If your max sites > 10000, add to your wp-config.php :
 
 ```php
 // Add msu max sites
--- wpg-multisite-share-users.php
+++ wpg-multisite-share-users.php
@@ -12,41 +12,36 @@
  * Tested up to:        4.2
  */
 
-if(!defined('MSU_MAX_SITES') && !function_exists('msu_add_to_all_sites') ) {
+namespace WPG\MultisiteShareUsers;
 
+if(!defined('MSU_MAX_SITES')){
 	define('MSU_MAX_SITES', 10000);
+}
 
-	function msu_add_to_all_sites( $user_id, $password='', $meta='' )  {
+function msu_add_to_all_sites( $user_id, $password='', $meta='' )  {
 
-		if(empty($meta)) {
-			$added_blog_id =get_active_blog_for_user( $user_id );
-			switch_to_blog($added_blog_id);
-			$role = get_userdata($user_id)->roles[0];
-			restore_current_blog();
-		}
-		else {
-			$added_blog_id = $meta['add_to_blog'];
-			$role = $meta['new_role'];
-		}
-
-		$sites = wp_get_sites(array('limit' => MSU_MAX_SITES));
-
-		foreach ( $sites AS $site ) {
-
-			if($site['blog_id'] != $added_blog_id) {
-
-				add_user_to_blog($site['blog_id'], $user_id, $role);
-
-			}
-
-		}
-
+	if(empty($meta)) {
+		$added_blog_id =get_active_blog_for_user( $user_id );
+		switch_to_blog($added_blog_id);
+		$role = get_userdata($user_id)->roles[0];
+		restore_current_blog();
+	}
+	else {
+		$added_blog_id = $meta['add_to_blog'];
+		$role = $meta['new_role'];
 	}
 
-	// When self-registred or when a super admin creates it
-	add_action( 'wpmu_new_user', 'msu_add_to_all_sites', 10, 1 );
-
-	// When admin of a subsite creates it
-	add_action( 'wpmu_activate_user', 'msu_add_to_all_sites', 10, 3 );
+	$sites = wp_get_sites(['limit' => MSU_MAX_SITES]);
+	foreach ( $sites AS $site ) {
+		if($site['blog_id'] != $added_blog_id) {
+			add_user_to_blog($site['blog_id'], $user_id, $role);
+		}
+	}
 
 }
+
+// When self-registred or when a super admin creates it
+add_action( 'wpmu_new_user', __NAMESPACE__ . '\\msu_add_to_all_sites', 10, 1 );
+
+// When admin of a subsite creates it
+add_action( 'wpmu_activate_user', __NAMESPACE__ . '\\msu_add_to_all_sites', 10, 3 );
